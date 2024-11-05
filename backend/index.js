4c91f399787a4dbd5634b5705e3e14cf8abfe803
Yousra El Jaafari El Idrissi <yeji1@alu.ua.es>
2024-11-05 15:51:56 +0100

    Inicio backend

---
+++ backend/index.js
@@ -0,0 +1,24 @@
+
+//Importación de módulos
+
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config()
+
+
+// Crear una aplicación de express
+const app = express();
+
+app.use(cors());  
+
+// Abrir la aplicacíon en el puerto 3000
+app.listen(process.env.PORT, () => {
+    console.log('Servidor corriendo en el puerto ' , process.env.PORT);
+});
+
+app.get('/', (req, res) => {
+    res.json({
+        ok: true,
+        msg: 'Hola mundo'
+    });
+});
