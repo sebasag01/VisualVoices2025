0f317f5b08dcda792804400ce87f1c8c9df494ea
Freemank71 <angelmruiz06@gmail.com>
2024-11-10 16:45:04 +0100

    Frontend y backend conectados

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,13 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from './services/api.service';
 import { RouterOutlet } from '@angular/router';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterModule], 
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'frontend';
+  response: any;
+
+  constructor(private apiService: ApiService) {}
+
+  ngOnInit() {
+    this.apiService.getHelloWorld().subscribe(
+      data => {
+        this.response = data;
+        console.log('Respuesta de la API:', this.response);
+      },
+      error => {
+        console.error('Error al obtener datos:', error);
+      }
+    );
+  }
 }
