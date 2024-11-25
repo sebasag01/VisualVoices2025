7d09a8e5d1b987079dda6130aa9ed7311ba5a3d5
Lorena Heras <lhc15@gcloud.ua.es>
2024-11-25 01:01:36 +0100

    creación componentes de login y registro

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegistroComponent } from './registro/registro.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: LoginComponent }, // Ruta raíz que carga LoginComponent
+  { path: 'registro', component: RegistroComponent } // Ruta del registro
+];
+
