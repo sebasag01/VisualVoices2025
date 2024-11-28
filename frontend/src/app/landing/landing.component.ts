70a28f17c4a78acb59bf11d81716c8284a1627ce
Lorena Heras <lhc15@gcloud.ua.es>
2024-11-28 19:55:07 +0100

    scroll y cambio de registro a login funcionando

--- frontend/src/app/landing/landing.component.ts
+++ frontend/src/app/landing/landing.component.ts
@@ -1,21 +1,45 @@
-import { Component } from '@angular/core';
-import { LoginComponent } from '../login/login.component'; // Ruta relativa al componente Login
-import { RegistroComponent } from '../registro/registro.component'; // Ruta relativa al componente Registro
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { LoginComponent } from '../login/login.component';
+import { RegistroComponent } from '../registro/registro.component';
 import { CommonModule } from '@angular/common';
-
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.css',
-  imports: [CommonModule,LoginComponent, RegistroComponent] // Importa los componentes standalone aquí
+  imports: [CommonModule, LoginComponent, RegistroComponent]
 })
 export class LandingComponent {
-  mostrarLogin: boolean = false;
+  isRegisterVisible: boolean = true;
 
-  alternarVista() {
-    this.mostrarLogin = !this.mostrarLogin;
+  @ViewChild('registerSection') registerSection!: ElementRef;
+  @ViewChild('loginSection') loginSection!: ElementRef;
+
+  // Mostrar la sección de registro y hacer scroll
+  showRegister(): void {
+    this.isRegisterVisible = true;
+
+    // Esperar un ciclo para que Angular actualice el DOM antes de desplazarse
+    setTimeout(() => {
+      this.scrollToSection(this.registerSection);
+    }, 0);
+  }
+
+  // Mostrar la sección de inicio de sesión y hacer scroll
+  showLogin(): void {
+    this.isRegisterVisible = false;
+
+    // Esperar un ciclo para que Angular actualice el DOM antes de desplazarse
+    setTimeout(() => {
+      this.scrollToSection(this.loginSection);
+    }, 0);
+  }
+
+  // Método para hacer scroll a la sección correspondiente
+  private scrollToSection(section: ElementRef): void {
+    if (section) {
+      section.nativeElement.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
   }
 }
