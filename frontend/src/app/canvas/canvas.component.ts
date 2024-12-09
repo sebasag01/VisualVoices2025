149911e7b702cd91cdf3b55b3a06754f1e9de26a
adrig907 <adrig907@gmail.com>
2024-12-09 18:11:49 +0100

    Configuracion motor

---
+++ frontend/src/app/canvas/canvas.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+
+
+@Component({
+  selector: 'app-canvas',
+  standalone: true,
+  imports: [],
+  templateUrl: './canvas.component.html',
+  styleUrls: ['./canvas.component.css'] 
+})
+export class CanvasComponent {
+
+}
